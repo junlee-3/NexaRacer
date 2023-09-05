ad8be1905595785042ceb31ebb02ec3a9a20e0b0
Jun3-dev <jun.lee73@outlook.com>
2023-09-06 09:43:20 +1000

    Setup SSID and password varibles. intilised steering and ESC controls and websocket

--- NexaRacer.ino
+++ NexaRacer.ino
@@ -5,3 +5,10 @@
 #include <WebSocketsClient.h>
 #include <Servo.h>
 
+const char* ssid = "Your_SSID";
+const char* password = "Your_PASSWORD";
+
+Servo steering;
+Servo ESC;
+
+WebSocketsClient webSocket;
