92b11d13849465d39cebb1b8996cb66608aadd8d
Jun3-dev <jun.lee73@outlook.com>
2023-09-06 10:00:41 +1000

    Setup web socket code, can now handle web socket and process requests

--- NexaRacer.ino
+++ NexaRacer.ino
@@ -12,3 +12,30 @@ Servo steering;
 Servo ESC;
 
 WebSocketsClient webSocket;
+
+void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
+    switch(type) {
+        case WStype_DISCONNECTED:
+            Serial.printf("[WSc] Disconnected!\n");
+            break;
+        case WStype_CONNECTED:
+            Serial.printf("[WSc] Connected to url: %s\n");
+            break;
+        case WStype_TEXT:
+            handleWebSocketMessage(payload, length);
+            break;
+    }
+}
+
+void handleWebSocketMessage(uint8_t* data, size_t len) {
+    String receivedMsg = String((char*)data);
+    if (receivedMsg.startsWith("steering:")) {
+        String value = receivedMsg.substring(9); // 9 is the length of "steering:"
+        int angle = value.toInt();
+        steering.write(angle);  // Adjust the steering servo based on the received angle
+    } else if (receivedMsg.startsWith("speed:")) {
+        String value = receivedMsg.substring(6); // 6 is the length of "speed:"
+        int speedValue = value.toInt();
+        ESC.write(speedValue);  // Adjust the speed ESC based on the received speed value
+    }
+}
